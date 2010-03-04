9f588eddf3e2ed435240d502f13c4c8d6eda28aa
glejeune <gregoire.lejeune@free.fr>
2010-03-04 19:51:19 +0100

    change indent in ceres.js

--- static/javascript/ceres.js
+++ static/javascript/ceres.js
@@ -1,39 +1,39 @@
 function feed_checking() {
-	new Ajax.Request('/ajax/feed/checking',
-	  {
-	    method:'get',
-	    onSuccess: function(transport){
-	      var response = transport.responseText || "no response text";
-	      if( response == "true" ) {
-					$('checking').show()
-					$('not_checking').hide()
-				} else {
-					$('checking').hide()
-					$('not_checking').show()
-				}
-	    },
-	    onFailure: function(){ alert('Something went wrong...') }
-	  });
+  new Ajax.Request('/ajax/feed/checking',
+    {
+      method:'get',
+      onSuccess: function(transport){
+        var response = transport.responseText || "no response text";
+        if( response == "true" ) {
+          $('checking').show()
+          $('not_checking').hide()
+        } else {
+          $('checking').hide()
+          $('not_checking').show()
+        }
+      },
+      onFailure: function(){ alert('Something went wrong...') }
+    });
 }
 
 function admin_feed() {
-	feed_checking()
-	b = setInterval(feed_checking, 10000);
+  feed_checking()
+  b = setInterval(feed_checking, 10000);
 }
 
 function admin_check_now() {
-	new Ajax.Request('/ajax/feed/check', {method:'get'})
-	feed_checking()
+  new Ajax.Request('/ajax/feed/check', {method:'get'})
+  feed_checking()
 }
 
 function admin_change_interval() {
-	
-	new Ajax.Request('/ajax/feed/interval/'+$('interval').value , 
-		{
-			method:'get',
-			onSuccess: function(transport){
-				var response = transport.responseText || "0";
-				$('interval').value = response
-			}
-		});
+  
+  new Ajax.Request('/ajax/feed/interval/'+$('interval').value , 
+    {
+      method:'get',
+      onSuccess: function(transport){
+        var response = transport.responseText || "0";
+        $('interval').value = response
+      }
+    });
 }
